326a4670e1a0e6adf9291d34793d10057c3aaa63
Osuolale1 <ayoolaoluwafemi@gmail.com>
2024-02-23 20:24:37 +0100

    Initial commit

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,24 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+require("@nomicfoundation/hardhat-toolbox");
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+const ALCHEMY_API_KEY = "XpWuAVYcHj7J0KOpMrd-c8Zr1Tdd0YJ5";
+
+// Replace this private key with your Sepolia account private key
+// To export your private key from Coinbase Wallet, go to
+// Settings > Developer Settings > Show private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Beware: NEVER put real Ether into testing accounts
+const SEPOLIA_PRIVATE_KEY = "8b2e54997c83fe1a298fc5b2978448a6f2b4b03ef30942279af3333e137b421c";
+
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: "PDGVZS82B1QWUB8IF17W3W8EVDPPBW13XX" // 
+  }
 };
-
-export default config;
